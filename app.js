d980023141ce690e35d9a30daf6f1c36b73604e5
NendiSofiandy <nendisofiandy80@gmail.com>
2026-02-09 11:45:12 +0700

    upate

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ eventBus.on('pmp2', (val) => {
         if(p.name == 'running_time') {
             const hours = Math.floor(p.value)
             writeInt16ToHR(8525, hours)
-            console.log(hours)
+            console.log(p)
         } else {
             writeInt32ToHR(map.reg, p.value)
         }
